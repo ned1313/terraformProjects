f0d3e627f4b4799900bed02cb76316e4fdd9c2cb
Ned Bellavance <Ned Bellavance>
2017-03-01 15:27:41 -0500

    Adding basic example

---
+++ BasicAWSExample/example.tf
@@ -0,0 +1,17 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0d729a60"
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
+}
